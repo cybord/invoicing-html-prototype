165f22148eb860a18959d2cfe8eb6a2dc2dc8755
Anthony Ringoet <anthonyringoet@gmail.com>
2014-01-28 15:19:44 +0100

    use extended input for add invoie

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 (function (window, document, $, undefined) {
   var $body = $('body');
 
-  $body.multiAdder();
+  $body.multiAdder()
+  $body.extendedInput({
+    hideClass: 'hide'
+  });
+
 })(window, document, jQuery);
